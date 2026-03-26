4c743f9e9dccdd07c6551af19de49ee133a8604d
Demetrius Ho Sang <demhos@graben-244.dynamic2.rpi.edu>
2026-03-26 12:49:52 -0400

    Changed Main to untilize Colab GPUs

--- src/train.py
+++ src/train.py
@@ -136,9 +136,10 @@ def train_one_epoch(model, dataloader, optimizer, device):
 def main():
     set_seed()
 
-    device = torch.device("cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
-    pin_memory = False
+
+    pin_memory = (device.type == "cuda")
 
     train_loader, val_loader = make_dataloaders(pin_memory)
 
